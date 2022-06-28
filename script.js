2a382dfb355bb5b8ab37c769b48e632cdbb3b733
Neenoe <adavaoluwatobisamuel@gmail.com>
2022-06-29 00:59:04 +0100

    you

---
+++ script.js
@@ -0,0 +1,21 @@
+// string
+var singleQuotes = 'Nino';
+var backticks = `I am ${singleQuotes}`
+
+console.log(backticks)
+
+let myWeight = '55';
+var kG = 'kg';
+var info = `I weigh ${myWeight}${kG}`
+
+console.log(info)
+
+var myCountry = 'Nigeria';
+var moreinfo = `I'm from ${myCountry}`
+
+console.log(moreinfo)
+
+
+
+
+
